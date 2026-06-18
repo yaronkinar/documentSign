9e2878efaf936dbd519351f5061560627778b535
yaronkinar <yaronkinar@gmail.com>
2026-06-18 15:01:49 +0300

    Resolve signer profile role titles per template

--- apps/api/src/signer-profiles/signer-profiles.module.ts
+++ apps/api/src/signer-profiles/signer-profiles.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { StorageModule } from '../storage/storage.module';
+import { PdfTemplate, PdfTemplateSchema } from '../templates/template.schema';
 import { SignerProfile, SignerProfileSchema } from './signer-profile.schema';
 import { SignerProfilesController } from './signer-profiles.controller';
 import { SignerProfilesService } from './signer-profiles.service';
@@ -10,6 +11,7 @@ import { SignerProfilesService } from './signer-profiles.service';
   imports: [
     MongooseModule.forFeature([
       { name: SignerProfile.name, schema: SignerProfileSchema },
+      { name: PdfTemplate.name, schema: PdfTemplateSchema },
     ]),
     StorageModule,
   ],
